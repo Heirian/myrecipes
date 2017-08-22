1743009805735225cec524c051dca219a18d8201
Heirian <shinkcc@gmail.com>
2017-08-22 13:37:46 -0300

    Add create comment to recipes show page

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  before_action :require_user
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @comment = @recipe.comments.build(comments_params)
+    @comment.chef = current_chef
+    if @comment.save
+      flash[:success] = "Comment was created successfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      flash[:danger] = "Comment wasn't created"
+      redirect_back(fallback_location: recipe_path(@recipe))
+    end
+  end
+
+  private
+
+  def comments_params
+    params.require(:comment).permit(:description)
+  end
+end

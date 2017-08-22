19bb681987ae121b7a24b9b22164f65176118ab6
Heirian <shinkcc@gmail.com>
2017-08-22 09:11:02 -0300

    Create Ingredients Controller

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,22 @@
+class IngredientsController < ApplicationController
+  def index
+    @ingredients = Ingredient.paginate(page: params[:page], per_page: 5)
+  end
+
+  def show
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+end
